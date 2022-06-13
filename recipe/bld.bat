239de2a185939a121365e855a0ce5b845947dfaf
mcflugen <mcflugen@gmail.com>
2022-06-13 16:53:10 -0600

    use cmake --build

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,22 +3,23 @@ copy %RECIPE_DIR%\CMakeLists.txt %SRC_DIR%\mesas\sas\cdflib90
 mkdir mesas\sas\cdflib90\_build
 cd %SRC_DIR%\mesas\sas\cdflib90\_build
 
-cmake .. ^
+cmake ^
+  -LAH ^
   -G "%CMAKE_GENERATOR%" ^
   -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
   -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
   -DCMAKE_BUILD_TYPE:STRING=Release ^
-  -DCMAKE_Fortran_COMPILER=%FORTRAN_COMPILER%
+  -DCMAKE_Fortran_COMPILER=%FORTRAN_COMPILER% ^
+  ..
 if errorlevel 1 exit /b 1
 
 ::  -S %SRC_DIR%/mesas/sas/cdflib90/ ^
 ::  -B %SRC_DIR%/mesas/sas/cdflib90/_build
 
-nmake
+cmake --build . --config Release
 if errorlevel 1 exit /b 1
 
 cd %SRC_DIR%
-:: cmake --build %SRC_DIR%/mesas/sas/cdflib90/_build --config Release
 
 %PYTHON% -m pip install . -vv
 if errorlevel 1 exit /b 1
