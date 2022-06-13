a6a5eb99a78787406855f19b135a97c0f3ff7bc9
mcflugen <mcflugen@gmail.com>
2022-06-13 16:32:08 -0600

    run cmake from build folder

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,17 +1,19 @@
 copy %RECIPE_DIR%\CMakeLists.txt %SRC_DIR%\mesas\sas\cdflib90
 
 mkdir mesas\sas\cdflib90\_build
-cmake ^
+cd %SRC_DIR%\mesas\sas\cdflib90\_build
+
+cmake .. ^
   -G "%CMAKE_GENERATOR%" ^
   -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
   -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
   -DCMAKE_BUILD_TYPE:STRING=Release ^
-  -DCMAKE_Fortran_COMPILER=%FORTRAN_COMPILER% ^
-  -S %SRC_DIR%/mesas/sas/cdflib90/ ^
-  -B %SRC_DIR%/mesas/sas/cdflib90/_build
+  -DCMAKE_Fortran_COMPILER=%FORTRAN_COMPILER%
 if errorlevel 1 exit /b 1
 
-cd %SRC_DIR%\mesas\sas\cdflib90\_build
+::  -S %SRC_DIR%/mesas/sas/cdflib90/ ^
+::  -B %SRC_DIR%/mesas/sas/cdflib90/_build
+
 nmake
 if errorlevel 1 exit /b 1
 
